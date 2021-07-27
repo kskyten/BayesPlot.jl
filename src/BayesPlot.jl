2c88d4b916fcf95420a7faa4554c12f43b6417b8
Kusti Skytén <kskyten@users.noreply.github.com>
2021-07-27 21:37:26 +0300

    Add ppc_dens_overlay

--- src/BayesPlot.jl
+++ src/BayesPlot.jl
@@ -1,9 +1,19 @@
 module BayesPlot
+using Makie
 
 function mcmc_recover_hist(posterior, true_parameters; kwargs...)
 end
 
 function ppc_dens_overlay(y, yrep; kwargs...)
+    f = Figure()
+    Axis(f[1, 1])
+    density!(y, color=:transparent, strokecolor = :black, strokewidth=2)
+
+    for yr in yrep
+        density!(yr, color=:transparent, strokecolor=(:blue, 0.1), strokewidth=1)
+    end
+
+    return f
 end
 
 function ppc_rootogram(y, yrep; kwargs...)
