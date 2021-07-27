3f071a1f24f56cf131cebbebedd849cedadc734a
Kusti Skytén <kskyten@users.noreply.github.com>
2021-07-27 19:49:58 +0300

    Initial commit

--- src/BayesPlot.jl
+++ src/BayesPlot.jl
@@ -1,5 +1,36 @@
 module BayesPlot
 
-# Write your package code here.
+function mcmc_recover_hist(posterior, true_parameters; kwargs...)
+end
+
+function ppc_dens_overlay(y, yrep; kwargs...)
+end
+
+function ppc_rootogram(y, yrep; kwargs...)
+end
+
+function ppc_stat(y, yrep; kwargs...)
+end
+
+function ppc_intervals(y, yrep; kwargs...)
+end
+
+function ppc_stat_grouped(y, yrep; kwargs...)
+end
+
+function mcmc_hist(x; kwargs...)
+end
+
+function mcmc_trace(x; kwargs...)
+end
+
+function mcmc_scatter(x; kwargs...)
+end
+
+function mcmc_parcoord(x; kwargs...)
+end
+
+function mcmc_areas(x; kwargs...)
+end
 
 end
